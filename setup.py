28d9d87fa1d688ceeab8644da425aea57ac91cfa
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-07-26 12:21:59 +0200

    Initial structure
    * Add initial boilerplace of the repository.
    * Add initial module structure.
    * Add initial tests structure and configuration.
    
    Bug: T199079
    Change-Id: I115cce1daabd86b3463732820246fd1800a82bcb

---
+++ setup.py
@@ -0,0 +1,46 @@
+"""Package configuration."""
+
+from setuptools import find_packages, setup
+
+install_requires = [
+    'conftool>=1.0.1',
+    'cumin>=3.0.2',
+    'dnspython>=1.15.0',
+    'pyyaml>=3.11',
+    'requests>=2.11.1',
+]
+
+# Extra dependencies
+extras_require = {
+    # Test dependencies
+    'tests': [
+        'bandit>=1.1.0',
+        'flake8>=3.2.1',
+        'prospector[with_everything]>=0.12.4',
+        'pytest-cov>=1.8.0',
+        'pytest-xdist>=1.15.0',
+        'pytest>=3.0.3',
+    ],
+}
+
+setup_requires = [
+    'pytest-runner>=2.7.1',
+    'setuptools_scm>=1.15.0',
+]
+
+setup(
+    author='Riccardo Coccioli',
+    author_email='rcoccioli@wikimedia.org',
+    description='A library for automation and orchestration of tasks in the WMF infrastructure',
+    extras_require=extras_require,
+    install_requires=install_requires,
+    keywords=['wmf', 'automation', 'orchestration'],
+    license='GPLv3+',
+    name='spicerack',
+    packages=find_packages(exclude=['*.tests', '*.tests.*']),
+    platforms=['GNU/Linux'],
+    setup_requires=setup_requires,
+    use_scm_version=True,
+    url='https://github.com/wikimedia/operations-software-spicerack',
+    zip_safe=False,
+)
---
+++ spicerack/tests/unit/test_init.py
@@ -0,0 +1,6 @@
+"""Initialization tests."""
+
+
+def test_module_load():
+    """Loading the module should not raise exception."""
+    import spicerack  # noqa: F401
