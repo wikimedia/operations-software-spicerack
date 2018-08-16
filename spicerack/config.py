0e4321d881c21b9996dcca1874e8f7a7943d8a8a
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-08-16 16:46:35 +0200

    config: rename parameter to avoid negation
    Bug: T199079
    Change-Id: I6847957576fdeef2b013487032bf76225439754b

--- spicerack/config.py
+++ spicerack/config.py
@@ -11,18 +11,18 @@ SPICERACK_CONFIG_DIR = os.environ.get('SPICERACK_CONFIG_DIR', '/etc/spicerack')
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-def get_config(config_dir, no_raise=False):
+def get_config(config_dir, raises=True):
     """Parse a YAML config file and return it, without failing on error.
 
     Arguments:
         config_dir (str): the directory where to look for the configuration file to load.
-        no_raise (bool): whether to raise if unable to load the config.
+        raises (bool, optional): whether to raise exception if unable to load the config.
 
     Returns:
         dict: the parsed config or an empty dictionary as a fallback.
 
     Raises:
-        SpicerackError: if unable to load the configuration and ``no_raise`` is ``False``.
+        SpicerackError: if unable to load the configuration and ``raises`` is ``True``.
 
     """
     config_file = os.path.join(config_dir, 'config.yaml')
@@ -33,11 +33,10 @@ def get_config(config_dir, no_raise=False):
 
     except Exception as e:  # pylint: disable=broad-except
         message = "Could not load config file %s: %s"
-        if no_raise:
-            logger.debug(message, config_file, e)
-        else:
-            logger.error(message, config_file, e)
+        if raises:
             raise SpicerackError(repr(e)) from e
+
+        logger.debug(message, config_file, e)
 
     if config is None:
         config = {}
--- spicerack/tests/unit/test_config.py
+++ spicerack/tests/unit/test_config.py
@@ -16,38 +16,35 @@ def test_get_config_empty():
     assert {} == config_dict
 
 
-def test_get_config_invalid_raise(caplog):
+def test_get_config_invalid_raise():
     """Loading an invalid config should raise Exception."""
     config_dir = get_fixture_path('config', 'invalid')
     with pytest.raises(Exception):
         config.get_config(config_dir)
 
-    assert 'ERROR    Could not load config file' in caplog.text
-
 
 def test_get_config_invalid(caplog):
-    """Loading an invalid config with no_raise=True should return an empty dictionary."""
+    """Loading an invalid config with raises=False should return an empty dictionary."""
     config_dir = get_fixture_path('config', 'invalid')
     with caplog.at_level(DEBUG):
-        config_dict = config.get_config(config_dir, no_raise=True)
+        config_dict = config.get_config(config_dir, raises=False)
 
     assert {} == config_dict
     assert 'DEBUG    Could not load config file' in caplog.text
 
 
-def test_get_config_missing(caplog):
+def test_get_config_missing():
     """Loading a non-existent config should raise Exception."""
     config_dir = get_fixture_path('config', 'non-existent')
     with pytest.raises(Exception):
         config.get_config(config_dir)
-    assert 'ERROR    Could not load config file' in caplog.text
 
 
 def test_get_config_miss_no_raise(caplog):
-    """Loading a non-existent config with no_raise=True should return an empty dictionary."""
+    """Loading a non-existent config with raises=False should return an empty dictionary."""
     config_dir = get_fixture_path('config', 'non-existent')
     with caplog.at_level(DEBUG):
-        config_dict = config.get_config(config_dir, no_raise=True)
+        config_dict = config.get_config(config_dir, raises=False)
 
     assert {} == config_dict
     assert 'DEBUG    Could not load config file' in caplog.text
