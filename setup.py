6c05b39df8a5694b92004ff70cb013d1fa3afdb3
Riccardo Coccioli <rcoccioli@wikimedia.org>
2019-02-27 12:24:39 +0100

    Add support for Python 3.7
    Change-Id: Ie6457c33b98c5fc0ee38aaca76a5b441496c687a

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Clustering',
