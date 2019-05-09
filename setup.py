816e4b5d7c7eb70e0a8dd001d33b4519f0c8a4f1
Riccardo Coccioli <rcoccioli@wikimedia.org>
2019-05-09 18:32:52 +0200

    tests: temporarily force bandit < 1.6.0
    * Due to a bug upstream bandit 1.6.0 doesn't honor the excluded
      directories, causing the failure of the bandit tox environments.
      See https://github.com/PyCQA/bandit/issues/488
    * Temproarily forcing bandit to be < 1.6.0 until the fix is available
      upstream.
    
    Change-Id: I8627d2a4e0e4b7f604b4dea843b910bbc5d4de45

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ INSTALL_REQUIRES = [
 EXTRAS_REQUIRE = {
     # Test dependencies
     'tests': [
-        'bandit>=1.1.0',
+        'bandit>=1.1.0,<1.6.0',
         'flake8>=3.2.1',
         'flake8-import-order>=0.18.1',
         'mypy>=0.470',
