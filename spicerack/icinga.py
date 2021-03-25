88c83f3f2a583f0d69f5e64a1acb1b0960acd530
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-03-25 18:04:44 +0100

    icinga: simplify command_file detection
    Change-Id: I42b2830913c7bdd137d32dc8b93b586895028932

--- spicerack/icinga.py
+++ spicerack/icinga.py
@@ -52,7 +52,7 @@ class CommandFile(str):
 
         try:
             # Get the command_file value in the Icinga configuration.
-            command = r"""awk '/^\s*command_file=/{split($0, a, "="); print a[2] }' """ + config_file
+            command = r"""awk -F= '$1=="command_file" {print $2}' """ + config_file
             command_file = ""
             for _, output in icinga_host.run_sync(command, is_safe=True):  # Read only operation
                 command_file = output.message().decode().strip()
