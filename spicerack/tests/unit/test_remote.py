8d497695e122509b24adc265b9b1713af0ae42bd
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-05-12 13:04:14 +0200

    remote: increase reboot wait time
    * In some cases, in particular during reimages, the reboot time can take
      longer. Increase the limit for now as in most cases this will not
      change anything as the check will succeed way before the timeout.
    
    Bug: T307260
    Change-Id: Ifd849fd52d86982fb0967b080daf1610183e9616

--- spicerack/tests/unit/test_remote.py
+++ spicerack/tests/unit/test_remote.py
@@ -385,8 +385,8 @@ class TestRemoteHosts:
         ):
             self.remote_hosts.wait_reboot_since(since)
 
-        # wait_reboot_since() sets tries to 120 and dry_run is False.
-        assert mocked_uptime.call_count == 120
+        # wait_reboot_since() sets tries to 240 and dry_run is False.
+        assert mocked_uptime.call_count == 240
         assert mocked_sleep.called
 
     @mock.patch("wmflib.decorators.time.sleep", return_value=None)
