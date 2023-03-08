096d1bd3f8031966ec8d1bac7f3c55593be6d363
Riccardo Coccioli <rcoccioli@wikimedia.org>
2023-03-08 10:38:59 +0100

    setup.py: bump dependencies minimum version
    * As Spicerack minimum Debian supported version is currently Bullseye,
      bump the minimum versions of dependencies to the same version in
      Debian, when present.
    
    Change-Id: I4cc76388922ae897d987733e8c442613bc9fb5c9

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ with open("README.rst", "r") as readme:
 INSTALL_REQUIRES = [
     "conftool>=1.0.1",
     "cumin>=3.0.2",
-    "dnspython>=1.16.0,<2.2.0",  # Temporary upper limit to prevent mypy failures
+    "dnspython>=2.0.0",
     "elasticsearch>=5.0.0,<7.15.0",
     "elasticsearch-curator~=5.0",
     "gitpython>=3.1.14",
-    "kafka-python>=1.4.3",
+    "kafka-python>=2.0.1",
     "kubernetes==12.0.*",  # frozen to the version available on debian bullseye
     "packaging",
     "pymysql>=0.9.3",
     "pynetbox~=6.6",
-    "redis>=3.2.1,<=4.1.3",
-    "requests>=2.21.0",
+    "redis>=3.5.3,<=4.1.3",
+    "requests>=2.25.0",
     "wmflib",
 ]
 
@@ -27,20 +27,20 @@ INSTALL_REQUIRES = [
 EXTRAS_REQUIRE = {
     # Test dependencies
     "tests": [
-        "bandit>=1.5.1",
-        "flake8>=3.6.0",
-        "mypy>=0.670",
-        "pytest-cov>=2.6.0",
-        "pytest-xdist>=1.26.1",
-        "pytest>=3.10.1",
-        "requests-mock>=1.5.2",
+        "bandit>=1.6.2",
+        "flake8>=3.8.4",
+        "mypy>=0.812",
+        "pytest-cov>=2.10.1",
+        "pytest-xdist>=2.2.0",
+        "pytest>=6.0.2",
+        "requests-mock>=1.7.0",
         # This is required for flake8 to run proprely, as when running tox
         # setuptools comes boundled is usually way older (debian sid has 44 as
         # of writing this).
         "setuptools>=53.0",
         "sphinx_rtd_theme>=1.0",
-        "sphinx-argparse>=0.2.2",
-        "Sphinx>=1.8.4",
+        "sphinx-argparse>=0.2.5",
+        "Sphinx>=3.4.3",
         "types-PyMySQL",
         "types-redis",
         "types-requests",
@@ -53,21 +53,21 @@ EXTRAS_REQUIRE = {
     "prospector": [
         "prospector[with_everything]>=0.12.4,<=1.7.7",  # Temporary upper limit for an upstream regression
         "pylint<2.15.7",  # Temporary upper limit for a change that breaks prospector that can't be upgraded
-        "pytest>=3.10.1",
-        "requests-mock>=1.5.2",
+        "pytest>=6.0.2",
+        "requests-mock>=1.7.0",
     ],
 }
 
 SETUP_REQUIRES = [
-    "pytest-runner>=2.7.1",
-    "setuptools_scm>=1.15.0",
+    "pytest-runner>=2.11.1",
+    "setuptools_scm>=5.0.1",
 ]
 
 setup(
     author="Riccardo Coccioli",
     author_email="rcoccioli@wikimedia.org",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
